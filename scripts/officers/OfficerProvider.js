c38b95ffba91f4a111a7058956f96582558172d4
Ricky Blake <rab0196@yahoo.com>
2020-08-31 11:16:27 -0500

    completed officers

--- scripts/officers/OfficerProvider.js
+++ scripts/officers/OfficerProvider.js
@@ -1,14 +1,11 @@
 let officers = []
 
-export const useOfficers = () => {
-    return officers.slice()
-}
+export const useOfficers = () => officers.slice();
 
 export const getOfficers = () => {
     return fetch("https://criminals.glassdale.us/officers")
         .then(response => response.json())
-        .then(
-            parsedOfficers => {
+        .then(parsedOfficers => {
                 console.table(parsedOfficers)
                 officers = parsedOfficers
             }
