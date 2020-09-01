e6e88914d480512d66f585a0826071484d433a96
Ricky Blake <rab0196@yahoo.com>
2020-09-01 12:34:24 -0500

    got searchbar working

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -2,16 +2,36 @@
  *   ConvictionSelect component that renders a select HTML element
  *   which lists all convictions in the Glassdale PD API
  */
-import { useConvictions } from "./ConvictionProvider.js"
+import { useConvictions, getConvictions } from "./ConvictionProvider.js"
 
-// Get a reference to the DOM element where the <select> will be rendered
+const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".filters__crime")
+
+eventHub.addEventListener("change", event => {
+console.log(event)
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value,
+                crimeId: event.target.id
+            }
+        })
+        console.log(customEvent)
+        // Dispatch to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
 
 export const ConvictionSelect = () => {
     // Get all convictions from application state
-    const convictions = useConvictions()
-    convictions.sort(compare);
-    render(convictions)
+    getConvictions()
+        .then(() => {
+            const convictions = useConvictions()
+            convictions.sort(compare);
+            render(convictions)
+        })
 }
 
 const render = convictionsCollection => {
@@ -22,7 +42,7 @@ const render = convictionsCollection => {
             ${
                 convictionsCollection.map(crimeObj => {
                     return `
-                        <option value="crime-${crimeObj.id}">${crimeObj.name}</option>
+                        <option value="${crimeObj.name}">${crimeObj.name}</option>
                     `
                 })
             }
@@ -43,3 +63,5 @@ const compare = (a, b) => {
     }
     return comparison;
   }
+
+  
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,5 +1,25 @@
 import { getCriminals, useCriminals } from './CriminalProvider.js'
 import { CriminalHTML } from './Criminal.js'
+import { getConvictions } from '../convictions/ConvictionProvider.js'
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("crimeChosen", event => {
+    // You remembered to add the id of the crime to the event detail, right?
+    if ("crimeId" in event.detail) {
+        /*
+            Filter the criminals application state down to the people that committed the crime
+        */
+       getCriminals().then(() => {
+        const criminalArray = useCriminals()
+        const matchingCriminals = criminalArray.filter(criminal => {
+            return (criminal.conviction === event.detail.crimeThatWasChosen)
+        })
+
+        addCriminalsToDOM(matchingCriminals)
+    })
+    }
+})
 
 export const CriminalList = () => {
     getCriminals()
