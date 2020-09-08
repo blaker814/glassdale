f00c300e5000baceaa7c2f145a84152c327880bd
Ricky Blake <rab0196@yahoo.com>
2020-09-08 10:49:17 -0500

    refactored my alibi button and how it works

---
+++ scripts/criminals/AlibiDialog.js
@@ -0,0 +1,31 @@
+import { useCriminals } from "./CriminalProvider.js"
+
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("associatesClicked", event => {
+    const targetCriminal = useCriminals().find(criminal => {
+        return criminal.id === parseInt(event.detail.chosenCriminal)
+    })
+    
+    const alibiTarget = document.querySelector(`.alibiDialog--${targetCriminal.id}`)
+
+    if (alibiTarget.innerHTML === "") {
+        alibiTarget.innerHTML = `${
+            targetCriminal.known_associates.map(associate => {
+                return `
+                    <h4>${associate.name}</h4>
+                    <div>${associate.alibi}</div>
+                `
+            }).join("")
+        }`
+    } else {
+        alibiTarget.innerHTML = ""
+    }
+})
+
+export const AlibiDialog = id => {
+    return `
+        <span class="alibiDialog--${id}"></span>
+    `
+}
--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,3 +1,19 @@
+import { AlibiDialog } from "./AlibiDialog.js"
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", clickEvent => {
+    if(clickEvent.target.id.startsWith("associates--")) {
+        const [ btntag, criminalId ] = clickEvent.target.id.split("--")
+        const alibiEvent = new CustomEvent("associatesClicked", {
+            detail: {
+                chosenCriminal: criminalId
+            }
+        })
+        eventHub.dispatchEvent(alibiEvent)
+    }
+})
+
 export const CriminalHTML = criminalObj => {
     return `
         <section id="criminal-${criminalObj.id}" class="criminal-card">
@@ -6,7 +22,8 @@ export const CriminalHTML = criminalObj => {
             <p>Crime: ${criminalObj.conviction}</p>
             <p>Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
             <p>Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
-            <button id="associates--${criminalObj.id}">Associate Alibis</button>
+            <button id="associates--${criminalObj.id}" toggle>Associate Alibis</button>
+            ${AlibiDialog(criminalObj.id)}
         </section>
     `
 }
